32700f0089e71bbed0a434c4854551a71345dfea
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-01 19:39:50 -0400

    schema

--- models/subscriber.js
+++ models/subscriber.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose')
 
 const subscriberSchema = new mongoose.Schema({
   name:{
-
+    type: String,
+    required: True
   },
   subscribedTo: {
-
+    type: String,
+    required: True
   },
   subscribeDate: {
-    
+    type: Date,
+    required: True,
+    default: Date.now
   }
 })

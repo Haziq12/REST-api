2f24b6c23febcb948a15054cafb5a31a5b8754d3
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-01 19:16:20 -0400

    CRUD routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,6 +2,25 @@ const express = require('express')
 const router = express.Router()
 
 // Getting all subscribers
+router.get('/', (req, res) => {
+  res.send('Hello World')
+})
+// Get one subscriber
+router.get('/:id', (req, res) => {
+
+})
+// Creating subscriber
+router.post('/', (req, res) => {
+
+})
+// Update one subscriber
+router.patch('/:id', (req, res) => {
+
+})
+// Delete subscriber
+router.delete('/:id', (req, res) => {
+
+})
 
 module.exports = router
 
